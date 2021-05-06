a9313cc41d520940835a586c757041077774d11b
NoahSong <64300769+NoahSong@users.noreply.github.com>
2021-05-07 10:35:39 +1200

    separated time callback method into a class

---
+++ CVRPTW/Clients/OrTools/TimeCallback.cs
@@ -0,0 +1,60 @@
+using Google.OrTools.ConstraintSolver;
+using CVRPTW.Models.VehicleRouting;
+
+namespace CVRPTW.Clients.OrTools
+{
+    public class TimeCallback
+    {
+        private VehicleRoutingModel _dataset;
+        private RoutingIndexManager _manager;
+        private int[,] _timeMatrix;
+        private int[] _serviceTimeMatrix;
+        private FuelType? _fuelType;
+
+        public LongLongToLong Callback { get; set; }
+
+        public TimeCallback(VehicleRoutingModel dataset, RoutingIndexManager manager, int[,] timeMatrix, int[] serviceTimeMatrix = null, FuelType? fuelType = null)
+        {
+            _dataset = dataset;
+            _timeMatrix = timeMatrix;
+            _serviceTimeMatrix = serviceTimeMatrix;
+            _manager = manager;
+            _fuelType = fuelType;
+
+            if (serviceTimeMatrix != null && fuelType != null)
+            {
+                Callback = RunWithServiceTimeAndFuelType;
+            }
+            else if (serviceTimeMatrix != null && fuelType == null)
+            {
+                Callback = RunWithServiceTime;
+            }
+            else
+            {
+                Callback = Run;
+            }
+        }
+
+        private long Run(long fromIndex, long toIndex)
+        {
+            var fromNode = _manager.IndexToNode(fromIndex);
+            var toNode = _manager.IndexToNode(toIndex);
+            return _timeMatrix[fromNode, toNode];
+        }
+
+        private long RunWithServiceTime(long fromIndex, long toIndex)
+        {
+            var fromNode = _manager.IndexToNode(fromIndex);
+            var toNode = _manager.IndexToNode(toIndex);
+            return _timeMatrix[fromNode, toNode] + _serviceTimeMatrix[fromNode];
+        }
+
+        private long RunWithServiceTimeAndFuelType(long fromIndex, long toIndex)
+        {
+            //TODO: Calculate with the consideration of fuel type.
+            var fromNode = _manager.IndexToNode(fromIndex);
+            var toNode = _manager.IndexToNode(toIndex);
+            return _timeMatrix[fromNode, toNode] + _serviceTimeMatrix[fromNode];
+        }
+    }
+}
